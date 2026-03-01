d0f0dcf442b7c68e9408801e9b0228457c0dc512
2k22.cscys.2211334@gmail.com <2k22.cscys.2211334@gmail.com>
2026-03-01 06:56:45 +0530

    updated submodule

--- Day 7/Simple to-do-list/script.js
+++ Day 7/Simple to-do-list/script.js
@@ -1,9 +1,20 @@
 function addTask(){
-    const taskInput = document.getElementById('taskInpt');
+    const taskInput = document.getElementById('taskInput');
     //when task is button is clicked, the value is stored in the task-list.
-    
+    const taskList = document.getElementById('task-list');
+   const taskText = taskInput.value;
+   const li=document.createElement('li');
+   li.innerText=`${taskText}`;
 
+   //delete the task ...
+   const deletebtn=document.createElement('button');
+   deletebtn.innerText='Delete';
+   deletebtn.onclick=function(){
+    li.remove();
 
-
+   }
+   li.appendChild(deletebtn);
+   taskList.appendChild(li);
+   taskInput.value="";
 
 }
